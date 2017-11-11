7709e231a02226ca5f9130384979f0533cd0d3be
Mayank Bansal <sunny.bansal@gmail.com>
2017-11-11 15:45:51 -0600

    Added Angular

---
+++ js/altruistApp.js
@@ -0,0 +1,63 @@
+var altruistApp = {
+    pages: {
+        home: 0,
+        dashboard: 1,
+        catalog: 2,
+        account: 3
+    },
+    currentPage: null,
+    nodeURL: 'https://vivre.manky.me:3000/',
+    angular: angular.module("miApp", ["ngRoute"])
+};
+
+
+
+altruistApp.angular.config(["$locationProvider", function ($locationProvider) {
+    // hack for html5Mode customization
+    $('a').each(function () {
+        $a = $(this);
+        if ($a.is('[target]') || $a.is('[ng-href]')) {
+        } else {
+            $a.attr('target', '_self');
+        }
+    });
+
+    $locationProvider.html5Mode(true);
+}]);
+
+
+altruistApp.angular.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+    $routeProvider
+        .when("/", {
+            templateUrl: "templates/home.html",
+            controller: "homeController"
+        })
+        .when("/home", {
+            templateUrl: "templates/home.html",
+            controller: "homeController"
+        })
+        .when("/dashboard", {
+            templateUrl: "templates/dashboard.html",
+            controller: "dashboardController"
+        })
+        .when("/account", {
+            templateUrl: "templates/account.html",
+            controller: "accountController"
+        })
+        .when("/catalog", {
+            templateUrl: "templates/catalog.html",
+            controller: "catalogController"
+        })
+        .otherwise({
+            templateUrl: "templates/404.html",
+            controller: "pnfController"
+        });
+
+    $locationProvider.html5Mode({enabled: true, rewriteLinks: false});
+}]);
+
+
+altruistApp.angular.controller('miAppController', function ($scope, $http) {
+
+});
