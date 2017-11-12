7866e333479cf87240a61a49ba2dc0deda3b0a00
Karthic Lakshmi Rajakumar <klakshmirajakumar@hawk.iit.edu>
2017-11-12 07:41:40 -0600

    s

--- share/routes/users.js
+++ share/routes/users.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../app/models/user');
+var Post = require('../app/models/posts');
+
 
 
 /* GET users listing. */
 router.get('/me', function(req, res, next) {
   var token = req.decoded;
     User.findOne({email:token},function(err,docs){
+        if(docs.length ===0 ){
+            return res.json({success:false,result:[]});
+        }else{
+            return res.json({success:true,result:docs});
+        }
+    })
+    
+});
+
+router.get('/byMe', function(req, res, next) {
+  var token = req.decoded;
+    Post.find({email:token},function(err,docs){
         if(docs.length ===0 ){
             return res.json({success:false,result:[]});
         }else{
