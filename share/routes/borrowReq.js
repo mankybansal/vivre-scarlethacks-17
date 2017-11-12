f62095db6518e407b636e52be015667d79141bd8
Karthic Lakshmi Rajakumar <klakshmirajakumar@hawk.iit.edu>
2017-11-12 09:27:16 -0600

    s

--- share/routes/borrowReq.js
+++ share/routes/borrowReq.js
@@ -69,7 +69,7 @@ router.get('/',function(req,res){
 
 router.get('/lent',function(req,res){
     var email = req.decoded;
-    BorrowReq.find({"borrower.email":email},function(err,docs){
+    BorrowReq.find({$and:[{"borrower.email":email},{accepted:{$ne:"true"}}]},function(err,docs){
         if(err){
             return res.json({success:false,message:err});
         }else{
